7c1b85c04d402809b91824b7dbf143abfad031f9
Firja Fairuz Zakia <firjaafairuzzakia@gmail.com>
2026-06-13 17:50:28 +0700

    monitoring ini

--- app/src/main/java/ananda/yoga/projectuasmobile/fragment/MonitoringFragment.kt
+++ app/src/main/java/ananda/yoga/projectuasmobile/fragment/MonitoringFragment.kt
@@ -1,60 +1,111 @@
 package ananda.yoga.projectuasmobile.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.ListView
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import ananda.yoga.projectuasmobile.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MonitoringFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MonitoringFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var actCariPs: AutoCompleteTextView
+    private lateinit var spStatus: Spinner
+    private lateinit var lvMonitoring: ListView
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_monitoring, container, false)
+    ): View {
+
+        val view =
+            inflater.inflate(
+                R.layout.fragment_monitoring,
+                container,
+                false
+            )
+
+        actCariPs =
+            view.findViewById(R.id.actCariPs)
+
+        spStatus =
+            view.findViewById(R.id.spStatus)
+
+        lvMonitoring =
+            view.findViewById(R.id.lvMonitoring)
+
+        setupSpinner()
+
+        setupAutoComplete()
+
+        setupDummyData()
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MonitoringFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MonitoringFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupSpinner() {
+
+        val data = arrayOf(
+            "Semua",
+            "Tersedia",
+            "Digunakan",
+            "Maintenance"
+        )
+
+        val adapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                data
+            )
+
+        adapter.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
+        spStatus.adapter = adapter
+    }
+
+    private fun setupAutoComplete() {
+
+        val data = arrayOf(
+            "PS 1",
+            "PS 2",
+            "PS 3",
+            "PS 4",
+            "PS 5"
+        )
+
+        val adapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_dropdown_item_1line,
+                data
+            )
+
+        actCariPs.setAdapter(adapter)
+    }
+
+    private fun setupDummyData() {
+
+        val data = arrayOf(
+            "PS 1 | PS5 | Tersedia",
+            "PS 2 | PS4 | Digunakan",
+            "PS 3 | PS5 | Maintenance"
+        )
+
+        val adapter =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                data
+            )
+
+        lvMonitoring.adapter = adapter
     }
 }
